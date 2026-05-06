5ee4beaa988e9dee6f57de73f29ac3d02fdfe636
RenatoABecker <reaugustobe@gmail.com>
2026-05-06 17:28:41 -0300

    Add files via upload

---
+++ Trabalho/main.py
@@ -0,0 +1,16 @@
+import utils
+a =[]
+
+# na GUI quando tiver s/n pode colocar varios botoes que vao redirecionar para o que voce quer fazer
+# tipo 4 botao, um pra ler, pra criar, deletar e atualizar, ficando tudo em uma so tela ao inves de cada um
+# ficar te perguntando toda vez
+faz=0
+while faz != "sair":
+    print(" ")
+    print("""Oque voce quer fazer?          para encerrar digite sair""")
+    print("Criar | Ler | Atualizar | Deletar")
+    print(" ")
+
+    faz=input().lower()
+    if faz == "criar" or faz == "ler" or faz == "atualizar" or faz == "deletar":
+        eval(f"utils.{faz}(a)")
